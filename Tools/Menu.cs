c5fd2a4fd7b1d55567befbb84c06ca9bf66b0628
sec_goat <joshua.tilson@yahoo.com>
2013-03-01 07:35:55 -0500

    Found a way to work with dynamic menus across pages, i think. Play with that and then start building controllers and viewmodels for the different actions.

---
+++ Tools/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace RepairCustomerDatabase.Tools
+{
+    public static class Menu
+    {
+        //http://stackoverflow.com/questions/2844866/asp-net-mvc-create-dynamic-navigation-sub-menu-on-the-master-page
+        public static MvcHtmlString RenderMenu(this HtmlHelper html)
+        {
+            var page = Html.ViewContext.HttpContext.Request.RawUrl;
+
+        }
+    }
+}
